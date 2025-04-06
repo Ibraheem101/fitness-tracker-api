037bfe32effbb1e5f8c3860e37ddf7a54587f7a6
ibraheem101 <ayokeks@gmail.com>
2025-04-06 18:04:33 +0100

    Remove DB connection logic

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
-import os
-import time
-import psycopg2
 from flask import Flask
-from dotenv import load_dotenv
 
 app = Flask(__name__)
-
-url = os.getenv("DATABASE_URL")
-while True:
-    try:
-        connection = psycopg2.connect(url)
-        print("DB ready")
-        break
-    except psycopg2.OperationalError:
-        print("DB not ready. Retrying ...")
-        time.sleep(5)
 
 @app.get('/')
 def test():
