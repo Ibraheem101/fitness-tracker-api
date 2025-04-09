b65690b72e8a954a4bb4c09477cdf0be8a56d91b
ibraheem101 <ayokeks@gmail.com>
2025-04-09 21:17:09 +0100

    feat(auth): add test user creation route

---
+++ routes/auth.py
@@ -0,0 +1,20 @@
+from flask import jsonify, Blueprint, request
+from models.health_metrics import User #type:ignore
+from database.connection import SessionLocal #type:ignore
+
+auth_bp = Blueprint('auth', __name__)
+@auth_bp.route('/create-test-user', methods=['POST'])
+
+def create_user():
+    data = request.get_json()
+    user = User(
+        username = data['username'],
+        email = data['email'],
+        password_hash = data['password_hash']
+    )
+    session = SessionLocal()
+    session.add(user)
+    session.commit()
+    session.close()
+
+    return {"message": "User Created"}, 201
