8d3e71a02d82996353f680b6df064026976c4d80
ibraheem101 <ayokeks@gmail.com>
2025-04-09 21:29:23 +0100

    feat(api) register auth blueprint with /api prefix

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from routes.health import health_bp
+from routes.auth import auth_bp
 
 app = Flask(__name__)
+app.register_blueprint(auth_bp, url_prefix="/api")
+app.register_blueprint(health_bp, url_prefix="/api")
 
 @app.get('/')
 def test():
