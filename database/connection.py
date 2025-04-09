b166393c22d2377db5899bbcf64b351df568a0da
ibraheem101 <ayokeks@gmail.com>
2025-04-09 21:27:11 +0100

    feat: create db table with Base.metadata.create_all()

--- database/connection.py
+++ database/connection.py
@@ -1,18 +1,17 @@
 import os
-import time
-import psycopg2
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.health_metrics import Base #type:ignore
 
 load_dotenv()
-
 url = os.getenv("DATABASE_URL")
+if not url:
+    raise ValueError("DATABASE_URL is not set")
 
-def get_connection():
-    while True:
-        try:
-            connection = psycopg2.connect(url)
-            print("DB ready")
-            return connection
-        except psycopg2.OperationalError:
-            print("DB not ready. Retrying ...")
-            time.sleep(5)
+engine = create_engine(url)
+SessionLocal = sessionmaker(bind=engine)
+
+# Create tables (Not for prod)
+# Base.metadata.create_all(bind=engine)
+print("Tables created successfully.")
