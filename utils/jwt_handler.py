ceb502c4dda69ec6cd27fb8d05a603142efcde1a
ibraheem101 <ayokeks@gmail.com>
2025-04-10 23:04:33 +0100

    feat(jwt): add utility functions for jwt token creation and decoding

---
+++ utils/jwt_handler.py
@@ -0,0 +1,24 @@
+import os
+import jwt
+from dotenv import load_dotenv
+from datetime import datetime, timedelta, timezone
+
+load_dotenv()
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("JWT_SECRET_KEY is not set")
+
+def create_token(data: dict, expires_in: int = 60):
+    payload = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=expires_in)
+    payload.update({"exp": expire})
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256") #type:ignore
+
+def decode_token(token: str):
+    try:
+        decoded = jwt.decode(token)
+        return decoded
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
