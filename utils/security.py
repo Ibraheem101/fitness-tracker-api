630a603583391e0b198bb4ae7d8e854d1bf86ac5
ibraheem101 <ayokeks@gmail.com>
2025-04-15 10:25:34 +0100

    fix: correct typo in password hashing scheme

--- utils/security.py
+++ utils/security.py
@@ -1,6 +1,6 @@
 from passlib.context import CryptContext #type:ignore
 
-password_context = CryptContext(schemes=["bcrypy"], deprecated="auto")
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) ->str:
     return password_context.hash(password)
