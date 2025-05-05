66bb66a8fe0311748da880cb6e00aeb38906ee38
ibraheem101 <ayokeks@gmail.com>
2025-05-05 11:54:10 +0100

    feat(register): add frontend registration logic

---
+++ frontend/assets/js/register.js
@@ -0,0 +1,32 @@
+document.getElementById("login-form").addEventListener("submit", async function (e) {
+    e.preventDefault(); // prevent default form submit
+
+    const email = document.getElementById("email").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch("http://127.0.0.1:5000/api/create-user", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, email, password })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            // redirect to login
+            alert("Registration successful!");
+            window.location.href = "../frontend/index.html";
+        } else {
+            alert(data.error || "Registration failed");
+        }
+    } catch (error) {
+        console.error("Error during registration:", error);
+        alert("Something went wrong. Please try again.");
+    }
+
+
+});
